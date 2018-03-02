7cabc8ec621f6c2749a7ae19ee4f91ad42267dca
Nikolas Laskaris <laskarisn@gmail.com>
2018-03-02 19:10:57 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -6,3 +6,4 @@ create table port (id uuid primary key, name varchar(500) not null, longitude fl
 create table vessel (id uuid primary key, name varchar(500) not null, country uuid REFERENCES country(id), image bytea);
 create table visits (id uuid primary key, vessel uuid REFERENCES vessel(id), port uuid REFERENCES port(id), visittime timestamp);
 create table "user" (id uuid NOT NULL, full_name character varying(255), password character varying(255), role character varying(255), username character varying(255), CONSTRAINT user_pkey PRIMARY KEY (id));
+ALTER TABLE "user" ADD CONSTRAINT unique_username UNIQUE ("username");
